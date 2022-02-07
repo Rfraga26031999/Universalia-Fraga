df855da9c7bc5286d95bb46f391bd7f616afd681
Rfraga26031999 <rogeliofragaa@gmail.com>
2022-02-07 13:11:50 -0300

    Arreglo ruta static

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -127,4 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Universidades/static')]
